4bd27307a3fc5a432f005ce74bded47cf2ebe109
Mattias Schlenker <ms@mattiasschlenker.de>
2015-04-02 07:09:03 +0000

    add partially working webui

---
+++ build-deb/nuage-bridge/debian/usr/share/nuage/webui.py
@@ -0,0 +1,72 @@
+#!/usr/bin/python
+
+
+# This file is part of NuageWebUI - a port of YunWebUI to Debian 
+#
+# NuageWebUI is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+#
+# As a special exception, you may use this file as part of a free software
+# library without restriction.  Specifically, if other files instantiate
+# templates or use macros or inline functions from this file, or you compile
+# this file and link it with other files to produce an executable, this
+# file does not by itself cause the resulting executable to be covered by
+# the GNU General Public License.  This exception does not however
+# invalidate any other reasons why the executable file might be covered by
+# the GNU General Public License.
+#
+# Copyright 2015 Mattias Schlenker (http://www.mattiasschlenker.de/)
+
+
+from flask import Flask, request, redirect, url_for, send_from_directory
+import time
+import sys
+sys.path.insert(0, '/usr/lib/python2.7/bridge/')
+from bridgeclient import BridgeClient as bridgeclient
+
+app = Flask(__name__, static_url_path='/static', static_folder='static')
+brg = bridgeclient()
+
+@app.route('/')
+def default_route():
+    return "Hello World!"
+
+@app.route('/keystore_manager_example/<path:path>', methods=['GET'])
+def send_example(path):
+	return send_from_directory('static/keystore_manager_example', path)
+
+@app.route('/data/get')
+def bridge_get_all():
+        res = brg.getall()
+        arr = []
+        for k, v in res.iteritems():
+                arr.append('"' + k + '":"' + v + '"')
+        msg = '{"value":{' + ','.join(arr) + '},"response":"get"}'
+        return msg, 200, {'Content-Type': 'application/json; charset=utf-8'}
+
+@app.route('/data/get/<key>')
+def bridge_get_bykey(key):
+        res = brg.get(key)
+        msg = '{"value":"' + res + '","key":"' + key + '","response":"get"}'
+        return msg, 200, {'Content-Type': 'application/json; charset=utf-8'}
+
+@app.route('/data/put/<key>/<val>')
+def bridge_put(key, val):
+        res = brg.put(key, val)
+        msg = '{"value":"' + val + '","key":"' + key + '","response":"put"}'
+        return msg, 200, {'Content-Type': 'application/json; charset=utf-8'}
+
+if __name__ == "__main__":
+        app.run(host='0.0.0.0',port='8088')
+
